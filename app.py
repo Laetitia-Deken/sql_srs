234917529d2f9eb1d8032ed261dfee13876f2252
Laetitia-Deken <laetitia.deken@orange.fr>
2024-01-30 13:45:36 +0100

    Add database with exercises and memory_state

--- app.py
+++ app.py
@@ -5,77 +5,57 @@ import duckdb
 import pandas as pd
 import streamlit as st
 
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
-
-# Création de la donnée
-CSV2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-# Pandas dataframe
-food_items = pd.read_csv(io.StringIO(CSV2))
-
-# Ecriture de la réponse attendue par l'utilisateur pour que cela fonctionne
-ANSWER_STR = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
+con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 
 # Dataframe de résultat, prendre cette query et la mettre dans Duckdb SQL
-solution_df = duckdb.sql(ANSWER_STR).df()
+# solution_df = duckdb.sql(ANSWER_STR).df()
 
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ["Joins", "GroupBy", "Windows Functions"],
+        ["Cross-Joins", "GroupBy", "Windows Functions"],
         index=None,
         placeholder="Select a theme: ",
     )
-    st.write("You selected:", option)
+    st.write("You selected:", theme)
 
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
 
 st.header("Enter your code: ")
 query = st.text_area(label="Code SQL", key="user_input")
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-    # Mettre les colonnes du DF result dans le même ordre que celle de la solution pour les comparer
-    # Try = prévision d'une KeyError en fonction de ce qu'a écrit l'utilisateur
-    try:
-        result = result[[solution_df.columns]]
-        # Comparer notre résultat  avec la solution avec compare
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-
-    # Compare le nombre de lignes de différence avec shape
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"Your result has a {n_lines_difference} lines difference with the solution"
-        )
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-# Tables que l'utilisateur a à disposition
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
-
-# Table à part avec la réponse, qui permet de chercher
-with tab3:
-    st.write(ANSWER_STR)
+# if query:
+#     result = duckdb.sql(query).df()
+#     st.dataframe(result)
+#
+#     # Mettre les colonnes du DF result dans le même ordre que celle de la solution pour les comparer
+#     # Try = prévision d'une KeyError en fonction de ce qu'à écrit l'utilisateur
+#     try:
+#         result = result[[solution_df.columns]]
+#         # Comparer notre résultat  avec la solution avec compare
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("Some columns are missing")
+#
+#     # Compare le nombre de lignes de différence avec shape
+#     n_lines_difference = result.shape[0] - solution_df.shape[0]
+#     if n_lines_difference != 0:
+#         st.write(
+#             f"Your result has a {n_lines_difference} lines difference with the solution"
+#         )
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# # Tables que l'utilisateur a à disposition
+# with tab2:
+#     st.write("table: beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("expected:")
+#     st.dataframe(solution_df)
+#
+# # Table à part avec la réponse, qui permet de chercher
+# with tab3:
+#     st.write(ANSWER_STR)
